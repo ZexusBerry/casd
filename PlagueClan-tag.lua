fbc4b8918705da7b2ad6976c216cd8c194ef1910
ZexusBerry <153849215+ZexusBerry@users.noreply.github.com>
2024-11-14 08:16:14 +0200

    PlagueClan-tag.lua

--- PlagueClan-tag.lua
+++ PlagueClan-tag.lua
@@ -10,7 +10,7 @@ local function clantags()
 
         if menu.get_int("clantag") == 0 and clantag_enabled then
     
-        cheat.set_clantag("")    
+        cheat.set_clantag("ZexusFuckAll")    
         clantag_enabled = false;
     
         elseif menu.get_int("clantag") == 1 then 
@@ -38,7 +38,32 @@ local function clantags()
             " ",
             }
 
-           elseif menu.get_int("clantag") == 3 then 
+            elseif menu.get_int("clantag") == 2 then 
+    
+            local clantag_neopredelenu = {
+            " INTERIUM? ",
+            " SKEET.cc?  ",
+            " OSIRIS?   ",
+            " RAWETRIP?   ",
+            " EVOLVE.XUZ? ",
+            " FATALITY?   ",
+            " GAMESENSE?  ",
+            " W1NNER.PW?  ",
+            " MIDNIGHT?   ",
+            " XONE?      ",
+            " NEVERLOSE?  ",
+            " NIXWARE?    ",
+            " WEAVE.SU?   ",
+            " OTCV3?      ",
+            " AURORA?     ",
+            " ExtrimHack? ",
+            " FluidAim?   ",
+            " OTCV4?     ",
+            " Qo0?        ",
+            " SpirtHack?  ",
+            }
+
+            elseif menu.get_int("clantag") == 3 then 
 
             local clantag_Onetap = {
                 "",
@@ -68,31 +93,6 @@ local function clantags()
                 cheat.set_clantag(clantag_string)
                 old_clantag_string = clantag_string
             end
-    
-        elseif menu.get_int("clantag") == 2 then 
-    
-            local clantag_neopredelenu = {
-            " INTERIUM? ",
-            " SKEET.cc?  ",
-            " OSIRIS?   ",
-            " RAWETRIP?   ",
-            " EVOLVE.XUZ? ",
-            " FATALITY?   ",
-            " GAMESENSE?  ",
-            " W1NNER.PW?  ",
-            " MIDNIGHT?   ",
-            " XONE?      ",
-            " NEVERLOSE?  ",
-            " NIXWARE?    ",
-            " WEAVE.SU?   ",
-            " OTCV3?      ",
-            " AURORA?     ",
-            " ExtrimHack? ",
-            " FluidAim?   ",
-            " OTCV4?     ",
-            " Qo0?        ",
-            " SpirtHack?  ",
-            }
 
             local curtime = math.floor((i_global_vars.GetCurTime()) * 3.5)
             local clantag_string = clantag_neopredelenu[curtime % #clantag_neopredelenu + 1]
